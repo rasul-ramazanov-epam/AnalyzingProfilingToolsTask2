f6be8545c3de8eac1861074431be253204c9b089
Rasul Ramazanov <Rasul_Ramazanov@epam.com>
2024-05-10 16:16:41 +0300

    Add project files.

---
+++ ProfileSample/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProfileSample.DAL;
+using ProfileSample.Models;
+
+namespace ProfileSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var context = new ProfileSampleEntities();
+
+            var images = context.ImgSources
+                .Take(20)
+                .Select(item => new ImageModel
+                {
+                    Name = item.Name,
+                    Data = item.Data
+                })
+                .ToList();
+
+            return View(images);
+        }
+
+        public ActionResult Convert()
+        {
+            var files = Directory.GetFiles(Server.MapPath("~/Content/Img"), "*.jpg");
+
+            using (var context = new ProfileSampleEntities())
+            {
+                foreach (var file in files)
+                {
+                    using (var stream = new FileStream(file, FileMode.Open))
+                    {
+                        byte[] buff = new byte[stream.Length];
+
+                        stream.Read(buff, 0, (int) stream.Length);
+
+                        var entity = new ImgSource()
+                        {
+                            Name = Path.GetFileName(file),
+                            Data = buff,
+                        };
+
+                        context.ImgSources.Add(entity);
+                    }
+                }
+
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
